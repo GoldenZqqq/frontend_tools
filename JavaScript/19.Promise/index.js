af154e59df29f80b5668bbfbae4459dfaeb338de
GoldenZqqq <1361001127@qq.com>
2024-10-26 21:51:36 +0800

    feat: Promise.then方法基本实现

--- JavaScript/19.Promise/index.js
+++ JavaScript/19.Promise/index.js
@@ -5,9 +5,10 @@ const REJECTED = "rejected"
 class MyPromise {
   #state = "pending"
   #value
+  #handlers = []
   constructor(executor) {
     const resolve = val => {
-      this.#setState(FULFILLED, al)
+      this.#setState(FULFILLED, val)
     }
     const reject = reason => {
       this.#setState(REJECTED, reason)
@@ -24,12 +25,42 @@ class MyPromise {
     if (this.#state !== PENDING) return
     this.#value = value
     this.#state = state
+    // 在这里执行
+    this.#runTask()
+  }
+
+  #runTask() {
+    if (this.#state !== PENDING) {
+      this.#handlers.forEach(cb => cb())
+      this.#handlers = []
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    return new MyPromise((resolve, reject) => {
+      this.#handlers.push(() => {
+        try {
+          const cb = this.#state === FULFILLED ? onFulfilled : onRejected
+          const res = typeof cb === "function" ? cb(this.#value) : this.#value
+          resolve(res)
+        } catch (error) {
+          reject(error)
+        }
+      })
+      this.#runTask()
+    })
   }
 }
 
 const p = new MyPromise((resolve, reject) => {
   setTimeout(() => {
-    throw new Error("Error")
-  })
+    resolve(1)
+    // reject(2)
+  }, 1000)
 })
-console.log(p.state)
+
+p.then(null, err => {
+  console.log("[ err ] >", err)
+}).then(res => {
+  console.log("[ 第二个then ] >", res)
+})
